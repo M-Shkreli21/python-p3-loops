c4ba47c4882f4a99ff26003764a211d2d73f356a
Mark Shkreli <m.shkreli96@icloud.com>
2023-03-24 18:44:05 -0400

    Lab Done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int_list * int_list for int_list in int_list]
+    return int_list
+    
+square_integers([1,2,3,4,5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+            continue
+        elif num % 3 == 0:
+            print("Fizz")
+            continue
+        elif num % 5 == 0:
+            print("Buzz")
+            continue
+        else:
+            print(num)
+
+fizzbuzz()    
+
+#List Comprehension:
+#player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+#inch_heights = [height * 7920 for height in player_heights]
+#print(inch_heights)
